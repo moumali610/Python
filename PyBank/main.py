941c1d2cb75d9761c927d66e06fde9b05923c7bc
Meriedith Umali <meriedithumali@Meriediths-MacBook-Air.local>
2018-02-22 20:29:31 -0500

    updated hw

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,153 @@
-# Dataset 1
+import os
+import csv
+ 
+pyBank_csv = os.path.join("budget_data_1.csv")
+ 
+ 
+# Lists to store data
+date = []
+revenue = []
+rev_change = []
+ 
+with open(pyBank_csv, newline="") as csvfile:
+    csvreader= csv.reader(csvfile, delimiter=",")
+  
+
+
+# Loops from the second row
+    next(csvreader, None)
+
+    for row in csvreader:
+       date.append(row[0])
+       revenue.append(int(row[1]))
+       # print(row)
+       
+
+# Financial Analysis Report
+print("                            ")
+print("Financial Analysis")
+print("----------------------------")
+
+# Finding Total Months by finding lenth of dataset
+print("Total Months: $" + (str(len(date)))) 
+
+#Finding Total Revenue by find the sum of the Revenue List
+#print("Total Revenue: ")
+
+
+# TotalRevenue = 0
+def sum(revenue):
+    total = 0
+    for x in revenue:
+        total = total + x
+    return total
+
+TotalRevenue = sum(revenue)
+print("Total Revenue: " + str(TotalRevenue))
+
+
+# The average change in revenue between months over the entire period 
+
+for i in range(len(revenue) - 1):
+    rev_change.append(int(revenue[i + 1]) - int(revenue[i]))
+    avg_rev_change = sum(rev_change)/len(rev_change)
+    max_rev_change = max(rev_change)
+    min_rev_change = min(rev_change)
+
+    max_rev_change_date = str(date[rev_change.index(max(rev_change))+1])
+    min_rev_change_date = str(date[rev_change.index(min(rev_change))+1])
+   
+    
+#print(avg_rev_change)
+
+print("Average Revenue Change: $" + str(avg_rev_change))
+print("Greatest Increase in Revenue: " + max_rev_change_date + "($", max_rev_change,")")
+print("Greatest Decrease in Revenue: " + min_rev_change_date + "($", min_rev_change,")")
+# The greatest increase in revenue (date and amount) over the entire period
+
+
+# The greatest decrease in revenue (date and amount) over the entire period
+
+
+
+
+#avg_rev_change = 0
+#def sum(rev_change):
+#    avg_rev_change = sum(rev_change)/len(rev_change)
+#    for i in range(1, len(revenue)):
+#        revenue.append(revenue[i] - revenue [i-1])
+#    return avg_rev_change
+#print(avg_rev_change)
+#AverageChange = sum(rev_change)
+#print(AverageChange)
+
+# average = sum(numlist) / len(numlist)
+
+
 
 import os
 import csv
-
-pybank_csv = os.path.join("budget_data_1.csv")
-
+ 
+pyBank_csv = os.path.join("budget_data_2.csv")
+ 
+ 
 # Lists to store data
-Date = []
-Revenue = []
-
-with open(pybank_csv, newline="") as csvfile:
+date = []
+revenue = []
+rev_change = []
+ 
+with open(pyBank_csv, newline="") as csvfile:
     csvreader= csv.reader(csvfile, delimiter=",")
+  
+
+
+# Loops from the second row
+    next(csvreader, None)
+
     for row in csvreader:
-        date.append(row[0])
-        revenue.append(row[1])
-        
-        
-        
-        
+       date.append(row[0])
+       revenue.append(int(row[1]))
+       # print(row)
+       
+
+# Financial Analysis Report
+print("                            ")
+print("Financial Analysis")
+print("----------------------------")
+
+# Finding Total Months by finding lenth of dataset
+print("Total Months: " + (str(len(date)))) 
+
+#Finding Total Revenue by find the sum of the Revenue List
+#print("Total Revenue: ")
+
+
+# TotalRevenue = 0
+def sum(revenue):
+    total = 0
+    for x in revenue:
+        total = total + x
+    return total
+
+TotalRevenue = sum(revenue)
+print("Total Revenue: $" + str(TotalRevenue))
+
+
+# The average change in revenue between months over the entire period 
+
+for i in range(len(revenue) - 1):
+    rev_change.append(int(revenue[i + 1]) - int(revenue[i]))
+    avg_rev_change = sum(rev_change)/len(rev_change)
+    max_rev_change = max(rev_change)
+    min_rev_change = min(rev_change)
+
+
+    max_rev_change_date = str(date[rev_change.index(max(rev_change))+1])
+    min_rev_change_date = str(date[rev_change.index(min(rev_change))+1])
+  
+
+print("Average Revenue Change: $" + str(avg_rev_change))
+print("Greatest Increase in Revenue: " + max_rev_change_date + "($", max_rev_change,")")
+print("Greatest Decrease in Revenue: " + min_rev_change_date + "($", min_rev_change,")")
+
+
--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,34 +1,53 @@
 import os
 import csv
-from US_abbrev import US_abbrev as USAb
 
-pyBoss_csv = os.path.join("employee_data1.csv")
+# from US_abbrev.py saved in same directory as main.py, import us_state_abbrev = {} , set it as variable
+from US_abbrev import us_state_abbrev as US_abbrev
 
-# Lists to store data
-Emp_ID = []
-Name = []
-dob = []
-SSN = []
-State = []
+# Paths to collect data from
+employee_data_path1 = os.path.join('employee_data1.csv')
+employee_data_path2 = os.path.join('employee_data2.csv')
 
-with open(pyBoss_csv, newline="") as csvfile:
-    csvreader= csv.reader(csvfile, delimiter=",")
+# Create List to store data and ["Add Headers"]
+ID = ["Employee ID"]
+firstName = ["First Name"]
+lastName = ["Late Name"]
+DoB = ["DoB"]
+SSN = ["SSN"]
+State = ["State"]
 
-    # Loops from the second row
-    next(csvreader, None)
-    
-    for row in csvreader:
-       Emp_ID.append(row[0])
-       Name.append(row[1])
-       dob.append(row[2])
-       SSN.append(row[3])
-       State.append(row[4])
-       #print(row)
+# Opening Dataset 1 and using [indexes] and split() to select the specific objects you want
+with open(employee_data_path1, newline="") as csvfile:
+	csvreader = csv.reader(csvfile,delimiter=",")
 
-# Current List Format:
-    # Emp ID,Name,DOB,SSN,State
-    # 214,Sarah Simpson,1985-12-04,282-01-8166,Florida
-    
-# Convert and export the data to use the following format instead:
-   # Emp ID,First Name,Last Name,DOB,SSN,State
-   # 214,Sarah,Simpson,12/04/1985,***-**-8166,FL
+	next(csvreader,None)
+	for row in csvreader:
+		ID.append(row[0])
+		firstName.append(row[1].split()[0])
+		lastName.append(row[1].split()[1])
+		DoB.append(row[2].split("-")[1]+"/"+row[2].split("-")[2]+"/"+row[2].split("-")[0])
+		SSN.append("***-**-"+row[3].split("-")[2])
+		State.append(US_abbrev[row[4]])
+
+# Opening Dataset 2 and using [indexes] and split() to select the specific objects you want
+with open(employee_data_path2, newline="") as csvfile:
+	csvreader = csv.reader(csvfile,delimiter=",")
+
+	next(csvreader,None)
+	for row in csvreader:
+		ID.append(row[0])
+		firstName.append(row[1].split()[0])
+		lastName.append(row[1].split()[1])
+		DoB.append(row[2].split("-")[1]+"/"+row[2].split("-")[2]+"/"+row[2].split("-")[0])
+		SSN.append("***-**-"+row[3].split("-")[2])
+		State.append(US_abbrev[row[4]])
+
+csv_file_path_new = os.path.join('employee_data_new.csv')
+
+
+# Open the output file
+with open(csv_file_path_new,mode="w", newline="") as csvfile:
+	csvwriter = csv.writer(csvfile,delimiter=",")
+	# Zip Lists together
+	for row in zip(ID,firstName,lastName,DoB,SSN,State):
+		csvwriter.writerow(row)
